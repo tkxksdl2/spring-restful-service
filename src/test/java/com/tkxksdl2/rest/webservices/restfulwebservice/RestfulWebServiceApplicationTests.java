b22ba13585831c2cbf5990d3300e70df1314f832
tkxksdl2 <tkxksdl2@gmail.com>
2024-01-02 12:13:01 +0900

    Initial: first commit

---
+++ src/test/java/com/tkxksdl2/rest/webservices/restfulwebservice/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkxksdl2.rest.webservices.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
