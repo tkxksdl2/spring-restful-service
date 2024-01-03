2247444103506ee0d851fd7f7d37121d40c306dd
tkxksdl2 <tkxksdl2@gmail.com>
2024-01-03 10:23:14 +0900

    feature: Swagger-ui, I18n messages

--- src/main/java/com/tkxksdl2/rest/webservices/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/tkxksdl2/rest/webservices/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,17 +1,32 @@
 package com.tkxksdl2.rest.webservices.restfulwebservice.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/hello-world")
     public String helloWorld(){
         return "helloWorld";
+    }
+
+    @GetMapping("/hello-world-i18n")
+    public String helloWorldI18n(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null,"Default Message", locale);
     }
 
     @GetMapping("/hello-world-bean")
